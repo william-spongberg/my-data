82a433038f22504b0084192cf64a863635efaec8
William Spongberg <william.spongberg@outlook.com>
2025-01-26 17:38:09 +1100

    Remove example files, update routes

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,37 +4,23 @@
 
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
-import * as $api_instagram_activity_activity from "./routes/api/instagram/activity/activity.ts";
-import * as $api_instagram_ads_ads from "./routes/api/instagram/ads/ads.ts";
-import * as $api_instagram_utils from "./routes/api/instagram/utils.ts";
 import * as $api_joke from "./routes/api/joke.ts";
-import * as $greet_name_ from "./routes/greet/[name].tsx";
 import * as $index from "./routes/index.tsx";
 import * as $platforms_instagram_instagram from "./routes/platforms/instagram/instagram.tsx";
-import * as $platforms_instagram_interfaces from "./routes/platforms/instagram/interfaces.tsx";
 import * as $Counter from "./islands/Counter.tsx";
-import * as $FileUpload from "./islands/FileUpload.tsx";
 import type { Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
-    "./routes/api/instagram/activity/activity.ts":
-      $api_instagram_activity_activity,
-    "./routes/api/instagram/ads/ads.ts": $api_instagram_ads_ads,
-    "./routes/api/instagram/utils.ts": $api_instagram_utils,
     "./routes/api/joke.ts": $api_joke,
-    "./routes/greet/[name].tsx": $greet_name_,
     "./routes/index.tsx": $index,
     "./routes/platforms/instagram/instagram.tsx":
       $platforms_instagram_instagram,
-    "./routes/platforms/instagram/interfaces.tsx":
-      $platforms_instagram_interfaces,
   },
   islands: {
     "./islands/Counter.tsx": $Counter,
-    "./islands/FileUpload.tsx": $FileUpload,
   },
   baseUrl: import.meta.url,
 } satisfies Manifest;
