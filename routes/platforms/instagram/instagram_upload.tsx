9172b31ec6a191797576fc1a724b78e75c463a8a
William Spongberg <william.spongberg@outlook.com>
2025-01-27 12:00:03 +1100

    Put file upload in session storage for holding between pages

--- routes/platforms/instagram/instagram_upload.tsx
+++ routes/platforms/instagram/instagram_upload.tsx
@@ -1,24 +1,22 @@
 // rename this file to something else? dont love /instagram/instagram tbh
 
 // TODO: allow zip folder uploads using https://deno.land/x/zip@v1.2.5
-// TODO: enforce file types, max file size, etc
 // TODO: drag and drop files instead - use package for this
 // TODO: make file uploading a reusable component
 
-import { Handlers, PageProps } from "$fresh/server.ts";
-import { FileData } from "../../../components/instagram/interfaces.tsx";
-import { handleFileData } from "../../../components/instagram/utils.tsx";
+// TODO: move to new page after uploading file and processing data - show analytics
+// TODO: show graphs, tables, etc (word cloud? would be cool to grab the logos of companies and show them in a word cloud)
 
-interface Props {
-  message: string | null;
-}
+import { Handlers } from "$fresh/server.ts";
+import { FileData } from "../../../components/instagram/interfaces.tsx";
+import { convertFileData } from "../../../components/instagram/utils.tsx";
 
 // file is uploaded through POST request, handled here
-export const handler: Handlers<Props> = {
+export const handler: Handlers = {
   // ignore get requests
   async GET(_req, ctx) {
     return await ctx.render({
-      message: null,
+      fileName: null,
     });
   },
   async POST(req, ctx) {
@@ -26,9 +24,10 @@ export const handler: Handlers<Props> = {
     const file = form.get("user-file") as File;
 
     // check if file exists
+    // TODO: enforce file types, max file size, etc
     if (!file) {
       return ctx.render({
-        message: `Please try again`,
+        fileName: `Please try again`,
       });
     }
 
@@ -39,17 +38,29 @@ export const handler: Handlers<Props> = {
       type: file.type,
     };
 
-    // do analytics and processing stuff
-    handleFileData(fileData);
+    // parse fileData
+    const instagramData = convertFileData(fileData);
 
-    return ctx.render({
-      message: `[${fileData.name}] uploaded!`,
-    });
+    if (!instagramData) {
+      return ctx.render({
+        fileName: `Invalid file`,
+      });
+    }
+
+    // using session storage - sec vulnerability to just pass data through URL
+    // TODO: may change to KV store later
+    sessionStorage.setItem("instagramData", JSON.stringify(instagramData));
+    
+    // go to new page to show analytics
+    const url = new URL(req.url);
+    url.pathname = "/platforms/instagram/instagram_analytics";
+
+    // 303 status code to redirect to new page (POST-redirect-GET pattern)
+    return Response.redirect(url.toString(), 303);
   },
 };
 
-export default function Upload(props: PageProps<Props>) {
-  const { message } = props.data;
+export default function Upload() {
   return (
     <div class="px-4 py-8 mx-auto bg-[#86efac]">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
@@ -62,7 +73,6 @@ export default function Upload(props: PageProps<Props>) {
           <input type="file" name="user-file" class="mb-4"/>
           <button type="submit" class="px-4 py-2 bg-blue-500 text-white rounded">Upload</button>
         </form>
-        {message ? <p>{message}</p> : null}
       </div>
     </div>
   );
