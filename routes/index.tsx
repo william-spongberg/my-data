9beeb7b318a405b331c856a1de273cc3455aa24e
William Spongberg <william.spongberg@outlook.com>
2025-02-10 16:26:46 +1100

    Added padding to home page to ensure visibility on mobile devices

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,13 +4,13 @@ import Footer from "../components/Footer.tsx";
 export default function Home() {
   return (
     <div class="flex flex-col min-h-screen bg-white">
-      <div class="flex-grow flex items-center justify-center">
-        <div class="px-4 py-8 mx-auto bg-blue-500 rounded-2xl">
-          <div class="max-w-screen-md mx-auto flex flex-col items-center">
-            <h1 class="text-4xl font-bold text-white">
+      <div class="flex-grow flex items-center justify-center px-4 sm:px-8">
+        <div class="px-8 py-8 mx-auto bg-blue-500 rounded-2xl w-full sm:w-auto">
+          <div class="max-w-screen-md px-4 mx-auto flex flex-col items-center">
+            <h1 class="text-2xl sm:text-4xl font-bold text-white text-center">
               What is my data worth?
             </h1>
-            <p class="text-lg text-white mt-4 mb-4">
+            <p class="text-base sm:text-lg text-white mt-4 mb-4 text-center">
               Welcome to your own data.
             </p>
             <Button
