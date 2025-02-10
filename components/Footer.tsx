ba34bfd57b4b78dc2cf107ad040e5d801f8f5bc2
William Spongberg <william.spongberg@outlook.com>
2025-02-10 11:38:19 +1100

    Moved Home button to Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,37 @@
+import Button from "./Button.tsx";
 
-export default function Footer() {
+interface Props {
+  home?: boolean;
+}
+
+export default function Footer({ home = false }: Props) {
   return (
-    <footer className="flex justify-center items-center h-16 bg-black text-white">
-      <p className="text-yellow-500">This website is in beta.</p>
-      <p className="mx-2">|</p>
-      <p>Made with ❤️ by <a href="https://github.com/william-spongberg" className="text-blue-500">William Spongberg</a></p>
-      <p className="mx-2">|</p>
-      <p>&copy; William Spongberg {new Date().getFullYear()}. All rights reserved.</p>
-    </footer>
+    <>
+      {!home && (
+        <Button
+          href="/"
+          name="Go back Home"
+        />
+      )}
+      <footer className="flex justify-center items-center h-16 bg-black text-white">
+        <br />
+        <p className="text-yellow-500">This website is in beta.</p>
+        <p className="mx-2">|</p>
+        <p>
+          Made with ❤️ by{" "}
+          <a
+            href="https://github.com/william-spongberg"
+            className="text-blue-500 hover:underline my-4"
+          >
+            William Spongberg
+          </a>
+        </p>
+        <p className="mx-2">|</p>
+        <p>
+          &copy; William Spongberg{" "}
+          {new Date().getFullYear()}. All rights reserved.
+        </p>
+      </footer>
+    </>
   );
 }
