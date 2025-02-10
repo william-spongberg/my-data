790a7cd2d3fb0231d4d633bcffbe7edfff2706a7
William Spongberg <william.spongberg@outlook.com>
2025-02-10 16:33:25 +1100

    Removed fresh logo from 404 page

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -1,4 +1,5 @@
 import { Head } from "$fresh/runtime.ts";
+import Button from "../components/Button.tsx";
 
 export default function Error404() {
   return (
@@ -6,20 +7,16 @@ export default function Error404() {
       <Head>
         <title>404 - Page not found</title>
       </Head>
-      <div class="px-4 py-8 mx-auto bg-[#86efac]">
+      <div class="px-4 py-8 mx-auto bg-[#5b68ad2d]">
         <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-          <img
-            class="my-6"
-            src="/logo.svg"
-            width="128"
-            height="128"
-            alt="the Fresh logo: a sliced lemon dripping with juice"
-          />
           <h1 class="text-4xl font-bold">404 - Page not found</h1>
           <p class="my-4">
-            The page you were looking for doesn't exist.
+            The page you were looking for does not exist.
           </p>
-          <a href="/" class="underline">Go back home</a>
+          <Button
+            href="/"
+            name="Go back home"
+          />
         </div>
       </div>
     </>
