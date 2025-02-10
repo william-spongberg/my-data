8761cd7061c1569c70fb719913af75a81d6782d5
William Spongberg <william.spongberg@outlook.com>
2025-02-10 16:12:38 +1100

    Greatly simplified Instagram Impression classes

--- classes/instagram/Ads/Impressions/Impressions.tsx
+++ classes/instagram/Ads/Impressions/Impressions.tsx
@@ -1,28 +1,61 @@
-import { RenderType } from "../../../../types/global/types.ts";
-import AdImpressions from "./AdImpressions.tsx";
-import PostImpressions from "./PostImpressions.tsx";
-import VideoImpressions from "./VideoImpressions.tsx";
+import { DataType, FileData } from "../../../../types/global/types.ts";
+import { Impression } from "../../../../types/instagram/types.ts";
+import LineChart from "../../../../islands/LineChart.tsx";
+import { randColour } from "../../../../utils/utils.ts";
 
-export default class Impressions implements RenderType {
-  ads_viewed: AdImpressions = new AdImpressions();
-  posts_viewed: PostImpressions = new PostImpressions();
-  videos_watched: VideoImpressions = new VideoImpressions();
+export default class Impressions implements DataType {
+  impressions: Impression[] = [];
+  title: string = "Impressions";
+  section: any;
+
+  constructor(fileData?: FileData, title?: string, section?: any) {
+    if (fileData) {
+      this.parse(fileData);
+    }
+    if (title) {
+      this.title = title;
+    }
+    if (section) {
+      this.section = section;
+    }
+  }
 
   render() {
+    if (this.impressions.length === 0) {
+      return (
+        <p>
+          No {(this.title).toLowerCase()} found.
+        </p>
+      );
+    }
     return (
       <>
-        <p class="text-xl">Ads and Topics</p>
-        {this.ads_viewed?.render()}
-        <br />
-        {this.posts_viewed?.render()}
-        <br />
-        {this.videos_watched?.render()}
+        <p>{this.title}</p>
+        <p class="text-sm italic">
+          {`You have seen ${this.impressions.length} impressions`}
+        </p>
+        <LineChart
+          id={this.title}
+          datasets={[{
+            label: this.title,
+            data: this.impressions,
+            color: randColour(),
+          }]}
+        />
       </>
     );
   }
 
-  // for later if needed
-  //posts_not_interested: PostsNotInterested;
-  //profiles_not_interested: ProfilesNotInterested;
-  //suggested_profiles: SuggestedProfiles;
+  parse(fileData: FileData) {
+    const jsonData = JSON.parse(fileData.text)[this.section];
+    this.impressions = jsonData.map((
+      impression: any,
+    ) => {
+      const stringMapData = impression.string_map_data;
+      return {
+        author: stringMapData.Author?.value || "Unknown",
+        timestamp: stringMapData.Time?.timestamp || "Unknown",
+      } as Impression;
+    });
+  }
 }
