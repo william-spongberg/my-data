8cf2422a1edfb1219cd15fb0ff31a6c7cc275441
William Spongberg <william.spongberg@outlook.com>
2025-02-16 15:36:41 +1100

    Updated 404 page

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -1,5 +1,7 @@
 import { Head } from "$fresh/runtime.ts";
 import Button from "../components/Button.tsx";
+import Page from "../components/Page.tsx";
+import * as Text from "../components/Text.tsx";
 
 export default function Error404() {
   return (
@@ -7,18 +9,17 @@ export default function Error404() {
       <Head>
         <title>404 - Page not found</title>
       </Head>
-      <div class="px-4 py-8 mx-auto bg-[#5b68ad2d]">
-        <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-          <h1 class="text-4xl font-bold">404 - Page not found</h1>
-          <p class="my-4">
-            The page you were looking for does not exist.
-          </p>
-          <Button
-            href="/"
-            text="Go back home"
-          />
-        </div>
-      </div>
+      <Page home={true}>
+        <Text.Title>404 - Page not found</Text.Title>
+        <br />
+        <Text.Paragraph>
+          The page you were looking for does not exist.
+        </Text.Paragraph>
+        <Button
+          href="/"
+          text="Go back home"
+        />
+      </Page>
     </>
   );
 }
